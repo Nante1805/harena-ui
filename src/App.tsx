5e80040566daa37307798671783c57c5db70d829
Nante1805 <hei.nantenaina.2@gmail.com>
2024-07-28 14:12:13 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Admin, Resource, ShowGuesser } from "react-admin";
+import { dataProvider } from './dataProvider';
+
+import { PostList, PostEdit, PostCreate } from "./posts";
+import { UserList } from "./users";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+import { Dashboard } from './Dashboard';
+
+export const App = () => (
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+
+   <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} show={ShowGuesser} icon={UserIcon}  />
+  </Admin>
+);
